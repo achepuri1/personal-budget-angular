564ca2fe9e325e04b2467bc1b967950e278f2fb3
Anjali <anjali@Anjalis-Air.lan>
2024-02-04 18:50:33 -0500

    loading data from json file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/',express.static('public'));
+
+app.get('/hello',(req,res)=>{
+    res.send('Hello World !');
+});
+const budget = require('./budget-data.json')
+
+app.get('/budget',(req,res)=>{
+    res.json(budget);
+});
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
